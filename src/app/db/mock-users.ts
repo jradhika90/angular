df4cb4546d604de33e21ac0a2e84ac269aa142c5
jradhika90 <jradhika90@gmail.com>
2020-04-05 03:07:12 +1000

    Created a mock users file for keeping sample data

---
+++ src/app/db/mock-users.ts
@@ -0,0 +1,54 @@
+import { User } from "../models/user.model";
+
+export const USERS: User[] = [
+  {
+    name: Ada,
+    email: ada@test.com,
+    password: ada,
+    state: VIC,
+    country: Australia,
+    postcode: 123456,
+    timePreference: Morning,
+    tnc: True
+  },
+  {
+    name: Babu,
+    email: babu@test.com,
+    password: babu,
+    state: Tamilnadu,
+    country: India,
+    postcode: 987654,
+    timePreference: Morning,
+    tnc: False
+  },
+  {
+    name: Casey,
+    email: casey@test.com,
+    password: ada,
+    state: New South Wales,
+    country: Australia,
+    postcode: 123564,
+    timePreference: Morning,
+    tnc: Boolean
+  },
+  {
+    name: Dia,
+    email: dia@test.com,
+    password: dia,
+    state: Westbengal,
+    country: India,
+    postcode: 897650,
+    timePreference: Morning,
+    tnc: True
+  },
+  {
+    name: Elvin,
+    email: elvin@test.com,
+    password: elvin,
+    state: New South Wales,
+    country: Australia,
+    postcode: 125634,
+    timePreference: Evening,
+    tnc: Boolean
+  }
+];
